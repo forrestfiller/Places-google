18c94baf91d34c056d58b5cd1c32bb8c9c705c18
Forrest Filler <forrestfiller@users.noreply.github.com>
2016-07-11 13:27:14 -0400

    added UITextField to header of tableView with shouldReturn delegate func

---
+++ Places-google/Model/Venue.swift
@@ -0,0 +1,18 @@
+//
+//  Venue.swift
+//  Places-google
+//
+//  Created by Forrest Filler on 7/11/16.
+//  Copyright © 2016 forrestfiller. All rights reserved.
+//
+
+import UIKit
+
+class Venue: NSObject {
+    
+    var name = ""
+    var address = ""
+    var lat: Double!
+    var lng: Double!
+
+}
--- Places-google/ViewController.swift
+++ Places-google/ViewController.swift
@@ -8,7 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UITextFieldDelegate {
+    
+    var venueTable: UITableView!
+    var searchField: UITextField!
+    var venueList = Array<Venue>()
+    
+    override func loadView() {
+        let frame = UIScreen.mainScreen().bounds
+        let view = UIView(frame: frame)
+        view.backgroundColor = .whiteColor()
+        
+        self.searchField = UITextField(frame: CGRect(x: 0, y: 0, width: frame.size.width, height: 44))
+        searchField.backgroundColor = .lightGrayColor()
+        self.searchField.delegate = self
+        
+        self.venueTable = UITableView(frame: frame, style: .Plain)
+        self.venueTable.dataSource = self
+        self.venueTable.delegate = self
+        self.venueTable.tableHeaderView = searchField
+        view.addSubview(self.venueTable)
+        
+        self.view = view
+    }
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +44,40 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
 
     }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.venueList.count
+        
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let venue = self.venueList[indexPath.row]
+        let cellId = "cellId"
+        if let cell = tableView.dequeueReusableCellWithIdentifier(cellId){
+            cell.textLabel?.text = venue.name
+            cell.detailTextLabel?.text = venue.address
+            return cell
+        }
+        let cell = UITableViewCell(style: .Subtitle, reuseIdentifier: cellId)
+        cell.textLabel?.text = venue.name
+        cell.detailTextLabel?.text = venue.address
+        return cell
+    }
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        print("textFieldShouldReturn: ")
+        textField.resignFirstResponder()
+        let searchText = self.searchField.text!
+        self.venueList.removeAll()
+        
+        return true
+    }
 
 
+    
+    
+    
+    
+    
+    // Fin! ....
 }
 
